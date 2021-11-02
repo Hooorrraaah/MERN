584d6fa6c4c8c417af05a9ba699e7ac39b3d2a3a
Austen Jensen <Austen.jensen@ymail.com>
2021-11-02 10:39:24 -0700

    update

--- React/hook/src/components/hook.js
+++ React/hook/src/components/hook.js
@@ -6,110 +6,47 @@ const UserForm = (props) => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
-    const [error, setError] = useState("");
 
 
-    const validateFN= (e) => {
-        setFirstName(e.target.value);
-        if(e.target.value.length < 1) {
-            setError(""); 
-        } else if(e.target.value.length < 2) {
-            setError("First name must be at least 2 characters or longer!");
-        }else{
-        setError("");
+    const newUser = (e) => {
+        e.preventDefault();
     }
-    };
-    const validateLN= (e) => {
-        setLastName(e.target.value);
-        if(e.target.value.length < 1) {
-            setError("");
-        } else if(e.target.value.length < 2) {
-            setError("Last name must be at least 2 characters or longer!");
-        }else{
-        setError("");
-    }
-    };
-    const validateE= (e) => {
-        setEmail(e.target.value);
-        if(e.target.value.length < 1) {
-            setError("");
-        } else if(e.target.value.length < 5) {
-            setError("Email must be at least 5 characters or longer!");
-        }else{
-        setError("");
-    }
-    };
-    const validateP= (e) => {
-        setPassword(e.target.value);
-        if(e.target.value.length < 1) {
-            setError("");
-        } else if(e.target.value.length < 8) {
-            setError("Password must be at least 8 characters or longer!");
-        }else{
-        setError("");
-    }
-    };
-    const validateCP= (e) => {
-        setConfirmPassword(e.target.value);
-        if(e.target.value.length < 1) {
-            setError("");
-        } else if(e.target.value != password.target.value) {
-            setError("Passwords must match!");
-        }else{
-        setError("");
-    }
-    };
+
     return(
-        <form onSubmit={ (e) => e.preventDefault() }>
+        <form onSubmit={ newUser }>
             <div>
             <label>First Name: </label>
-            <input type="text" onChange={ validateFN } />
-                {
-                    error ?
-                    <p style={{color:'red'}}>{ error }</p> :
-                    ''
-                }
+            <input type="text" onChange={ (e) => setFirstName(e.target.value) } value={ firstName } />
             </div>
+
             <div>
             <label>Last Name: </label>
-            <input type="text" onChange={ validateLN } />
-                {
-                    error ?
-                    <p style={{color:'red'}}>{ error}</p> :
-                    ''
-                }
+            <input type="text" onChange={ (e) => setLastName(e.target.value) } value={ lastName } />
             </div>
+
             <div>
             <label>Email: </label>
-            <input type="text" onChange={ validateE } />
-                {
-                    error ?
-                    <p style={{color:'red'}}>{ error }</p> :
-                    ''
-                }
+            <input type="text" onChange={ (e) => setEmail(e.target.value) } value={ email } />
             </div>
+
             <div>
             <label>Password: </label>
-            <input type="text" onChange={ validateP } />
-                {
-                    error ?
-                    <p style={{color:'red'}}>{ error }</p> :
-                    ''
-                }
+            <input type="text" onChange={ (e) => setPassword(e.target.value) } value={ password } />
             </div>
+
             <div>
             <label>Confirm Password: </label>
-            <input type="text" onChange={ validateCP } />
-                {
-                    error ?
-                    <p style={{color:'red'}}>{ error }</p> :
-                    ''
-                }
+            <input type="text" onChange={ (e) => setConfirmPassword(e.target.value) } value={ confirmPassword } />
             </div>
-            <input type="submit" value="Create User" />
+
+            <h3> User Information:</h3>
+            <p>First name: {firstName}</p>
+            <p>Last name: {lastName}</p>
+            <p>Email: {email}</p>
+            <p>Password: {password}</p>
+            <p>Confirm Password: {confirmPassword}</p>
         </form>
     );
-    
 };
 
 export default UserForm
