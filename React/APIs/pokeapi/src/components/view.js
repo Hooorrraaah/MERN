0d29e9602a23f7aa7515702ec47b29f9ab00f0f7
Austen Jensen <Austen.jensen@ymail.com>
2021-11-08 13:21:08 -0800

    update

--- React/APIs/pokeapi/src/components/view.js
+++ React/APIs/pokeapi/src/components/view.js
@@ -1,26 +1,35 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
 const View = (props) =>{
     const [poke, setPoke] = useState([]);
 
+    // useEffect(() => {
+    //     axios.get("https://pokeapi.co/api/v2/pokemon/1")
+    //     .then(res=>{
+    //         setPoke([res.data])
+    //     })
+    //     .catch(err=>{console.log(err)})
+    // }, [props.poke])
 
     function callAPI() {
-        axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807")
-        .then(response=>{
-                setPoke([response.data])
-            })}
+        axios.get("https://pokeapi.co/api/v2/pokemon/?limit=807/")
+        .then(res=>{
+            // console.log(res.data.results)
+            setPoke(res.data.results)
+        console.log(poke)
+    })
 
-
-
+    .catch(err=>{console.log(err)})
+        }
 
     return(
         <div>
             <h1>Poke API</h1>
             <ul>
             <button onClick={callAPI}>Fetch pokemon</button>
-                {poke.map((pokemon,i)=>{
-                    return <li key={i}>{pokemon.name}</li>
+                {poke.map((pokemon)=>{
+                    return <li>{pokemon.name}</li>
                 })}
             </ul>
         </div>
