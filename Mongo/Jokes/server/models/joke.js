cb29a33fef103e6851a323961423b0af638ba359
Austen Jensen <Austen.jensen@ymail.com>
2021-11-09 15:52:07 -0800

    update

--- Mongo/Jokes/server/models/joke.js
+++ Mongo/Jokes/server/models/joke.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const JokeSchema = new mongoose.Schema({
-    setup: String,
-    punchline: String
+    setup: {
+        type: String,
+        required: [true, "setup is required"],
+        minlength: [3, "Setup must be at least 3 characters long"]
+    },
+    punchline: {
+        type: String,
+        required: [true, "punchline is required"],
+        minlength: [10, 'Punchline must be at least 10 characters long']
+    }
 });
 
 const Joke = mongoose.model('Joke', JokeSchema);
